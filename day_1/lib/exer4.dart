3facbf8706ea01f9b748dfd26bbbc5bd495c72fc
ppfuark <phfuark2007@gmail.com>
2026-05-06 15:05:47 -0300

    Day 1[feat]: 4.2

--- day_1/lib/exer4.dart
+++ day_1/lib/exer4.dart
@@ -1,3 +1,4 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 class Exer4 extends StatefulWidget {
@@ -18,8 +19,144 @@ class _Exer4State extends State<Exer4> {
         width: w,
         height: h,
         padding: EdgeInsets.all(30),
-        child: SingleChildScrollView(child: Column(children: [])),
+        child: SingleChildScrollView(
+          child: Column(
+            spacing: 20,
+            children: [
+              Exercise(
+                name: "4.1 - File picker only for audios",
+                function: () async {
+                  try {
+                    FilePickerResult? result = await FilePicker.pickFiles(
+                      type: FileType.audio,
+                    );
+                    if (result != null) {
+                      PlatformFile file = result.files.first;
+                      if (context.mounted) {
+                        toast("Arquivo selecionado: ${file.name}", context);
+                        toast(
+                          "Arquivo selecionado: ${file.size.toString()}",
+                          context,
+                        );
+                        toast("Arquivo selecionado: ${file.path}", context);
+                      }
+                    } else {
+                      context.mounted
+                          ? toastError("Seleção de arquivo cancelada", context)
+                          : null;
+                    }
+                  } catch (e) {
+                    context.mounted
+                        ? toastError("Erro na seleção de arquivo: $e", context)
+                        : null;
+                  }
+                },
+              ),
+              Exercise(
+                name: "4.2 - File picker, custom type",
+                function: () async {
+                  try {
+                    FilePickerResult? result = await FilePicker.pickFiles(
+                      type: FileType.custom,
+                      allowedExtensions: ['mp3', 'aac', 'm4a'],
+                    );
+                    if (result != null) {
+                      PlatformFile file = result.files.first;
+                      if (context.mounted) {
+                        toast("Arquivo selecionado: ${file.name}", context);
+                        toast(
+                          "Arquivo selecionado: ${file.size.toString()}",
+                          context,
+                        );
+                        toast("Arquivo selecionado: ${file.path}", context);
+                      }
+                    } else {
+                      context.mounted
+                          ? toastError("Seleção de arquivo cancelada", context)
+                          : null;
+                    }
+                  } catch (e) {
+                    context.mounted
+                        ? toastError("Erro na seleção de arquivo: $e", context)
+                        : null;
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
+}
+
+class Exercise extends StatelessWidget {
+  final String name;
+  final Function function;
+  const Exercise({super.key, required this.name, required this.function});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(name, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+        Container(
+          width: double.infinity,
+          height: 80,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.circular(99),
+          ),
+          child: GestureDetector(
+            onTap: () {
+              function();
+            },
+            child: Center(
+              child: Text(
+                "Tap",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+void toast(String text, BuildContext context) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(
+        text,
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),
+      ),
+      backgroundColor: Colors.green,
+    ),
+  );
+}
+
+void toastError(String text, BuildContext context) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(
+        text,
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),
+      ),
+      backgroundColor: Colors.red,
+    ),
+  );
 }
